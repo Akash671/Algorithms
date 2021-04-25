e258326db53a78f2a5982f22ed568accabe70d1e
Akash Kumar <65735040+Akash671@users.noreply.github.com>
2021-04-25 16:03:34 +0530

    Add files via upload

---
+++ Permutaion_algorithm/string_purmutation.py
@@ -0,0 +1,14 @@
+import itertools
+ 
+if __name__ == '__main__':
+ 
+    nums =str(input())
+    S=nums
+    permutations = list(itertools.permutations(nums))
+ 
+    # Output: ['ABC', 'ACB', 'BAC', 'BCA', 'CAB', 'CBA']
+    a=[''.join(permutation) for permutation in permutations]
+    a.sort()
+    #j=0
+    for i in a:
+       print(i)
